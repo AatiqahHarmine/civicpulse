82738c5bce0cb39ab12bf24be251c78a78e65609
Aatiqah Harmine <aatiqahharmine27@gmail.com>
2026-06-13 14:43:55 +0530

    added render and vercel links

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,7 @@ dotenv.config();
 connectDB();
 
 const app = express();
-app.use(cors());
+app.use(cors({object:"https://civicpulse-green.vercel.app"}));
 app.use(express.json({ limit: '10mb' }));
 
 app.use('/api/auth', require('./routes/authRoutes'));
